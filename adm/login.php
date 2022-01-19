d65e1c7c1415fb7caf60b75a056d387f0ee7b6b1
Neemias Chagas de Souza Júnior <neemias.jr@gmail.com>
2022-01-19 11:41:55 -0300

    Adicionando try/catch no adm/login.php

--- adm/login.php
+++ adm/login.php
@@ -1,19 +1,18 @@
-<?php 
+<?php
 
 date_default_timezone_set('America/Sao_Paulo');
 
-if(!isset($_SESSION)){
-	session_start();
-	
+if (!isset($_SESSION)) {
+    session_start();
 }
 
 
 require './../lib/autoload.php';
 
 if (!empty($_ENV['REDIS_URL'])) {
-	$redisUrlParts = parse_url($_ENV['REDIS_URL']);
-	ini_set('session.save_handler', 'redis');
-	ini_set('session.save_path', "tcp://$redisUrlParts[host]:$redisUrlParts[port]?auth=$redisUrlParts[pass]");
+    $redisUrlParts = parse_url($_ENV['REDIS_URL']);
+    ini_set('session.save_handler', 'redis');
+    ini_set('session.save_path', "tcp://$redisUrlParts[host]:$redisUrlParts[port]?auth=$redisUrlParts[pass]");
 }
 
 
@@ -27,7 +26,7 @@ $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
 $smarty = new Template();
 
 
-if(isset($_POST['recovery'])):
+if (isset($_POST['recovery'])):
     
  
     // obejto USER
@@ -35,25 +34,27 @@ if(isset($_POST['recovery'])):
    // passo alguns valores
     $email = $_POST['txt_email'];
     $senha = Sistema::GerarSenha();
-    // verifico se tem este email na tabela 
-    if($user->GetUserEmail($email) > 0):
+    // verifico se tem este email na tabela
+    if ($user->GetUserEmail($email) > 0):
         
-        // faz alteração 
-        $user->AlterarSenha($senha, $email);  
-        
-          // apos alterar envia email com a nova senha
-         $enviar = new EnviarEmail();
-         
-         $assunto = 'Nova senha ADM do site '. Sistema::DataAtualBR();
-         $destinatarios = array($email,  $_ENV['SITE_EMAIL_ADM']);
-         $msg = ' Nova senha no ADM do site, nova senha:  ' .$senha;
-         
-         
-         $enviar->Enviar($assunto, $msg, $destinatarios);
-         
-           echo '<div class="alert alert-success"> Foi enviado um email com a NOVA SENHA  </div>';
-         
-         
+        try{
+          // faz alteração
+            $user->AlterarSenha($senha, $email);
+            
+            // apos alterar envia email com a nova senha
+            $enviar = new EnviarEmail();
+            
+            $assunto = 'Nova senha ADM do site '. Sistema::DataAtualBR();
+            $destinatarios = array($email,  $_ENV['SITE_EMAIL_ADM']);
+            $msg = ' Nova senha no ADM do site, nova senha:  ' .$senha;
+            
+            
+            $enviar->Enviar($assunto, $msg, $destinatarios);
+          
+            echo '<div class="alert alert-success"> Foi enviado um email com a NOVA SENHA  </div>';
+        } catch(Exception $e) {
+          echo "Erro ao enviar email: $e";
+        }
     else:
         
          echo '<div class="alert alert-danger"> Email não encontrado </div>';
@@ -63,15 +64,14 @@ if(isset($_POST['recovery'])):
 
 
 
-if(isset($_POST['txt_logar']) && isset($_POST['txt_email'])){
-	$user = $_POST['txt_email'];
-	$senha = $_POST['txt_senha'];
-	$login = new Login();
-	if($login->GetLoginADM($user, $senha)){
-		
-		Rotas::Redirecionar(0, 'index.php');
-		exit();
-	}
+if (isset($_POST['txt_logar']) && isset($_POST['txt_email'])) {
+    $user = $_POST['txt_email'];
+    $senha = $_POST['txt_senha'];
+    $login = new Login();
+    if ($login->GetLoginADM($user, $senha)) {
+        Rotas::Redirecionar(0, 'index.php');
+        exit();
+    }
 }
 
 
